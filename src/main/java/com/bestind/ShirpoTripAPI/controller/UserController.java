00c3bbc7233b8950e2811678c2edb90e2b3f5968
vvvvgross <mvv00122@outlook.com>
2024-03-03 14:35:44 +0300

    Сделал конвертор

--- src/main/java/com/bestind/ShirpoTripAPI/controller/UserController.java
+++ src/main/java/com/bestind/ShirpoTripAPI/controller/UserController.java
@@ -22,4 +22,42 @@ public class UserController {
             return ResponseEntity.badRequest().body("not ok");
         }
     }
+
+    //Place to JSON converter
+    public PlaceToJson(Place){
+        let jSonData = {
+                title = Place.title,
+                place_id = Place.place_id,
+                description = Place.descrition,
+                feedback = Place.feedback,
+                visitor_age = Place.visitor_age,
+                url = Place.url,
+                avarage_price = Place.avarage_price,
+                location = Place.location,
+                relax_type = Place.relaxType,
+                tags = Place.tags,
+                contact_info = Place.contact_info,
+                schedule = Place.schedule
+        };
+        return JSON.stringfy(jSonData);
+    }
+
+    //JSON to Place converter
+    public JsonToPlace(jSonData){
+        let place = {
+                title = jSonData.title,
+                place_id = jSonData.place_id,
+                description = jSonData.descrition,
+                feedback = jSonData.feedback,
+                visitor_age = jSonData.visitor_age,
+                url = jSonData.url,
+                avarage_price = jSonData.avarage_price,
+                location = jSonData.location,
+                relax_type = jSonData.relaxType,
+                tags = jSonData.tags,
+                contact_info = jSonData.contact_info,
+                schedule = jSonData.schedule
+        };
+        return Place;
+    }
 }
