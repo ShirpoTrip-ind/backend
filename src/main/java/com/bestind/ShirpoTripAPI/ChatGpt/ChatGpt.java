f9001542e7afaab613e8f8fc6a3b1fab4e57e2c3
nxlak <nick.wh1te@yandex.ru>
2024-03-18 02:11:46 +0300

    gpt integration

---
+++ src/main/java/com/bestind/ShirpoTripAPI/ChatGpt/ChatGpt.java
@@ -0,0 +1,16 @@
+package com.bestind.ShirpoTripAPI.ChatGpt;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatGpt {
+    public List<String> getTags(String text) {
+        List<String> tags = new ArrayList<>();
+
+        // получаем теги
+
+        return tags;
+    }
+}
