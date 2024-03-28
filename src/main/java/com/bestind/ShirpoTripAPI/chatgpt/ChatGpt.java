f3fa93a3f7081c8e7fe48b345ca381485e2067b5
nxlak <nick.white.94@bk.ru>
2024-03-28 21:28:50 +0300

    Добавил зависимость

--- src/main/java/com/bestind/ShirpoTripAPI/chatgpt/ChatGpt.java
+++ src/main/java/com/bestind/ShirpoTripAPI/chatgpt/ChatGpt.java
@@ -1,9 +1,20 @@
 package com.bestind.ShirpoTripAPI.chatgpt;
 
+import com.bestind.ShirpoTripAPI.config.ChatGptConfig;
+import com.bestind.ShirpoTripAPI.service.PlaceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ChatGpt {
+    private final ChatGptConfig extractTags;
+    @Autowired
+    ChatGpt(ChatGptConfig extractTags) {
+        this.extractTags = extractTags;
+    }
+
     public List<String> getTags(String text) {
         List<String> tags = new ArrayList<>();
 
